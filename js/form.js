299230931b5b3940d7bcbe3dd74740b730d81fd4
YOWASHI <129309849+yowashi-is-fish@users.noreply.github.com>
2023-06-02 21:09:57 +0900

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,3 +1,13 @@
 window.onload = function() {
   document.getElementsBy("reset").reset();
 }
+
+function check() {
+  if (window.confirm('送信してよろしいですか？')) {
+    submitted = true;
+    return true;
+  }
+  else {
+    return false;
+  }
+}
